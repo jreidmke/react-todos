8eccc6e18e9f5af6f0ebadbd2c28f1c2fd8772e9
unknown <jreidmke@gmail.com>
2020-12-02 11:31:12 -0600

    completed form testing

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -1,5 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+
 
 it('renders correctly', () => {
     render(<TodoForm/>)
@@ -9,3 +11,11 @@ it('matches snapshot', () => {
     const {asFragment} = render(<TodoForm/>);
     expect(asFragment()).toMatchSnapshot;
 });
+
+it('allows user to create new todo', () => {
+    const {getByLabelText, queryByText} = render(<TodoList/>);
+    expect(queryByText('Clean room!')).not.toBeInTheDocument();
+    fireEvent.change(getByLabelText("Task"), {target: {value: 'Clean room!'}});
+    fireEvent.click(queryByText('Add Todo'));
+    expect(queryByText('Clean room!')).toBeInTheDocument();
+})
